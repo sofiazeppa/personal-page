11b45fa632b10ed584fe48ffd5e3f3899ae5137f
Sofia <sofia.zeppa@outlook.com>
2019-01-07 20:09:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// // chart
+$(function() {
+    $('.chart').easyPieChart({
+      scaleColor: "#ecf0f1",
+      lineWidth: 20,
+      
+      barColor: '#FFA400', // color de la barra
+      trackColor:	"#ecf0f1",
+      size: 160,
+      animate: 500
+    });
+  });
+
